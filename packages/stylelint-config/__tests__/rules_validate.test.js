1fc8b4607f931167bbd02e55f735006b36c90768
mzn <841764743@qq.com>
2024-03-07 16:07:53 +0800

    feat: styleint

--- packages/stylelint-config/__tests__/rules_validate.test.js
+++ packages/stylelint-config/__tests__/rules_validate.test.js
@@ -2,15 +2,84 @@ const assert = require('assert');
 const stylelint = require('stylelint');
 const path = require('path');
 
-describe('test.rules.test.js',  () => {
-  it('validate default', async () => {
+describe('test/rules-validate.test.js', () => {
+  it('Validate default', async () => {
     const filePaths = [path.join(__dirname, './fixtures/index.css')];
 
     const result = await stylelint.lint({
-      configFile: path.join(__dirname, '../index.js'), // 规则的文件
-      files: filePaths, // 当前能接收的文件名
-      fix: false, // 是否自动修复
-    })
-    console.log(result);
-  })
-})
+      configFile: path.join(__dirname, '../index.js'),
+      files: filePaths,
+      fix: false,
+    });
+    console.log('执行了', result)
+    if (result && result.errored) {
+      const filesResult = JSON.parse(result.output || '[]') || [];
+      filesResult.forEach((fileResult) => {
+        console.log(`========= ${filePaths} ==========`);
+        console.log(fileResult.warnings);
+      });
+
+      assert.ok(filesResult.length !== 0);
+    }
+  });
+
+  it('Validate sass', async () => {
+    const filePaths = [path.join(__dirname, './fixtures/sass-test.scss')];
+
+    const result = await stylelint.lint({
+      configFile: path.join(__dirname, '../index.js'),
+      files: filePaths,
+      fix: false,
+    });
+
+    if (result && result.errored) {
+      const filesResult = JSON.parse(result.output || '[]') || [];
+      filesResult.forEach((fileResult) => {
+        console.log(`========= ${filePaths} ==========`);
+        console.log(fileResult.warnings);
+      });
+
+      assert.ok(filesResult.length !== 0);
+    }
+  });
+
+  it('Validate less', async () => {
+    const filePaths = [path.join(__dirname, './fixtures/less-test.less')];
+
+    const result = await stylelint.lint({
+      configFile: path.join(__dirname, '../index.js'),
+      files: filePaths,
+      fix: false,
+    });
+
+    if (result && result.errored) {
+      const filesResult = JSON.parse(result.output || '[]') || [];
+      filesResult.forEach((fileResult) => {
+        console.log(`========= ${filePaths} ==========`);
+        console.log(fileResult.warnings);
+      });
+
+      assert.ok(filesResult.length !== 0);
+    }
+  });
+
+  it('Validate css-module', async () => {
+    const filePaths = [path.join(__dirname, './fixtures/css-module.scss')];
+
+    const result = await stylelint.lint({
+      configFile: path.join(__dirname, '../index.js'),
+      files: filePaths,
+      fix: false,
+    });
+
+    if (result && result.errored) {
+      const filesResult = JSON.parse(result.output || '[]') || [];
+      filesResult.forEach((fileResult) => {
+        console.log(`========= ${filePaths} ==========`);
+        console.log(fileResult.warnings);
+      });
+
+      assert.ok(filesResult.length === 0);
+    }
+  });
+});
