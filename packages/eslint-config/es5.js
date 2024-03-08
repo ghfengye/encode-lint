4da4c1bca37fe54da87fb654024fbafa438ce668
mzn <841764743@qq.com>
2024-03-08 14:24:55 +0800

    feat: 制定规范，rules为不同维度的规范，放根目录下的为暴露给外部的各种组合

---
+++ packages/eslint-config/es5.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    './rules/base/best-practices',
+    './rules/base/possible-errors',
+    './rules/base/style',
+    './rules/base/variables',
+    './rules/base/es5',
+  ].map(require.resolve),
+  root: true, // 默认情况下，ESLint在所有父文件夹中查找配置文件，直到根目录。添加root为true，则找到配置文件后不再向上查找
+}
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    './rules/base/best-practices',
+    './rules/base/possible-errors',
+    './rules/base/style',
+    './rules/base/variables',
+    './rules/base/es6',
+    './rules/base/strict',
+    './rules/imports',
+  ].map(require.resolve),
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false, // babel需要使用babel配置文件，该配置告诉eslint，不用找这个配置文件
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      globalReturn: false,
+      impliedStrict: true,
+      jsx: true,
+    },
+  },
+  root: true,
+}
